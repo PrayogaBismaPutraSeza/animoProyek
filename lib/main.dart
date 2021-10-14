dee8dc7cab7181e3cc62378d3b5adffb6289cb11
PrayogaBismaPutraSeza <prayogabisma1@gmail.com>
2021-10-14 13:51:58 +0700

    coding 1.0

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:animo/view/signin.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff145C9E),
+        scaffoldBackgroundColor: Color(0xffF1F1F),
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SignIn(),
+    );
+  }
+}
+
---
+++ lib/view/signin.dart
@@ -0,0 +1,91 @@
+import 'package:animo/widgets/widget.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override 
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset("assets/images/logo.png", height: 50,),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height - 100,
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  style: simpleTextStyle(),
+                  decoration: textFieldInputDecoration("email"),
+                ),
+                TextField(
+                  decoration: textFieldInputDecoration("password"),
+                  ),
+                SizedBox(height: 8,),
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+                    child: Text("Forgot Password?", style: simpleTextStyle(),),
+                  ),
+                ),
+                SizedBox(height: 8,),
+                Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        const Color(0xff007EF4),
+                        const Color(0xff2A75BC)
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Text("Sign In", style: simpleTextStyle(),),
+                ),
+                SizedBox(height: 16,),
+                Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Text("Sign In with Google", style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 16,
+                  ),),
+                ),
+                SizedBox(height: 16,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don't have account? ", style: mediumTextStyle(),),
+                    Text("Register now",style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      decoration: TextDecoration.underline
+                    ),),
+                  ],
+                ),
+                SizedBox(height: 50,),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
