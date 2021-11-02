f855a608d82d875de62fdcf85cb40dd0bbd76881
bagasdewandana <muhammaddewandana@gmail.com>
2021-11-03 01:08:28 +0700

    Membuat chatroom

--- lib/view/search.dart
+++ lib/view/search.dart
@@ -1,4 +1,9 @@
+import 'dart:html';
+
+import 'package:animo/main.dart';
+import 'package:animo/services/database.dart';
 import 'package:animo/widgets/widget.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -7,33 +12,133 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+
+  DatabaseMethods databaseMethods = new DatabaseMethods();  
+  TextEditingController searchTextEditingController = new TextEditingController();
+
+  QuerySnapshot searchSnapshot;
+
+  initiateSearch(){
+    databaseMethods
+        .getUserByUsername(searchTextEditingController.text)
+        .then((val){
+      setState(() {
+              searchSnapshot = val;
+            });
+    });
+  }
+
+  /// Create chatroom, send user to conversation screen, pushreplacement
+  createChatRoomAndStartConversation(String userName){
+    
+    List<String> users = [userName,];
+    databaseMethods.createChatRoom();
+  }
+
+  Widget searchList(){
+    return searchSnapshot != null ? ListView.builder(
+      itemCount: searchSnapshot.documents.length,
+        shrinkWrap: true,
+        itemBuilder: (context, index){
+        return SearchTile(
+          userName: searchSnapshot.documents[index].data["name"],
+          userEMail: searchSnapshot.documents[index].data["email"],
+        );
+        }) : Container();
+  }
+
+  @override
+    void initState() {
+      initiateSearch();
+      super.initState();
+    }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBarMain(context),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      body: Container(      
         child: Column(
           children: [
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "search doctor",
-                      hintStyle: TextStyle(
-                        color: Colors.white54
+            Container(
+              color: Color(0x54FFFFFF),
+              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        hintText: "search doctor",
+                        hintStyle: TextStyle(color: Colors.white),
+                        border: InputBorder.none
                       ),
-                      border: InputBorder.none
+                    )
+                  ),
+                  GestureDetector(
+                    onTap: (){
+                      initiateSearch();
+                    },
+                    child: Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            const Color(0x36FFFFFF),
+                            const Color(0x0FFFFFFF)
+                          ]
+                        ),
+                        borderRadius: BorderRadius.circular(40)
+                      ),
+                      padding: EdgeInsets.all(12),
+                      child: Image.asset("assets/images/search_white.png"),
                     ),
                   )
-                ),
-
-                Image.asset("assets/images/search_white.png")
-              ],
-            )
+                ],
+              ),
+            ),
+            searchList()
           ],
         ),
+      ),
+    );
+  }
+}
+
+class SearchTile extends StatelessWidget {
+  final String userName;
+  final String userEMail;
+  SearchTile({this.userName, this.userEMail});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(userName, style: mediumTextStyle(),),
+              Text(userEMail, style: mediumTextStyle(),),
+            ],
+          ),
+          Spacer(),
+          GestureDetector(
+            onTap: (){
+
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(13)
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              child: Text("Message", style: mediumTextStyle(),),
+            ),
+          )
+        ],
       ),
     );
   }
